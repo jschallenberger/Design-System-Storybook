31d0ab10db31a6ef59766c7b1bc6c71f0cd4f4ec
Johann Schallenberger <schajohann@yahoo.com.br>
2022-10-14 21:28:01 -0300

    Fix syntax

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -17,7 +17,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION'){
-      config.base= '/Design-System-Storybook/'
+      config.base= '/design-system-storybook/'
     }
 
     return config
