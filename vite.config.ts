ed517ff0293fff687d668282089848197bb97ba7
Johann Schallenberger <schajohann@yahoo.com.br>
2022-10-15 01:45:55 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    minify: false,
-    sourcemap: false,
-  }
+  plugins: [react()]
 })
